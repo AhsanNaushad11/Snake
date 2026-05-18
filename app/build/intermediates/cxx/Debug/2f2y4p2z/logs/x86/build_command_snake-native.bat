17ff92fa2976e5810d2b2d8ad3dc7593196785a9
ahsannaushad11 <ahsannaushad@outlook.com>
2026-05-18 18:29:52 +0500

    Upgraded using AGP.

---
+++ app/build/intermediates/cxx/Debug/2f2y4p2z/logs/x86/build_command_snake-native.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\AhsanBinNaushad\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\AhsanBinNaushad\\Documents\\Codex\\2026-05-15\\create-a-snake-game-project-utilize\\app\\.cxx\\Debug\\2f2y4p2z\\x86" ^
+  snake-native
